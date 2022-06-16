fc04114b60d1ea8654a832ab0b6277972c2f72dd
Ayush Mandowara <ayushxx7@gmail.com>
2022-06-17 03:35:27 +0530

    feat: deployment - conditions for cleaner output
    ++Formatted files

--- app.py
+++ app.py
@@ -3,15 +3,22 @@ from model import get_sentiment_recommendations
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     user = request.form.get('user')
     recommendation_resp = get_sentiment_recommendations(user)
-    return render_template('index.html', user=user, recommendation=recommendation_resp)
+
+    if "doesn't exist" in recommendation_resp.lower():
+        return render_template('index.html', user=user)
+    else:
+        return render_template('index.html', user=user, recommendation=recommendation_resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model.py
+++ model.py
@@ -2,10 +2,12 @@ import joblib
 import pandas as pd
 import numpy as np
 
+# loading required objects
 train = joblib.load('dataset.gz')
 mnb = joblib.load('mnb.gz')
 user_final_rating = joblib.load('user_final_rating.gz')
 vectorizer = joblib.load('vectorizer.gz')
+
 
 def get_sentiment_recommendations(user):
     """
@@ -14,8 +16,10 @@ def get_sentiment_recommendations(user):
     - Extract total positive ratio to sort the products from most popular to least popular.
     """
     if (user in user_final_rating.index):
-        # get the product recommedation using the trained ML model
+        # get top recommendations
         recommendations = list(user_final_rating.loc[user].sort_values(ascending=False)[0:20].index)
+
+        # analyze sentiment of reviews for the recommended products
         temp = train[train.name.isin(recommendations)].copy()
         X = vectorizer.transform(temp["reviews_clean"].values.astype(str))
         temp["predicted_sentiment"] = mnb.predict(X)
@@ -25,6 +29,8 @@ def get_sentiment_recommendations(user):
         temp_grouped["total_review_count"] = temp_grouped['predicted_sentiment']
         temp_grouped['pos_sentiment_percent'] = np.round(temp_grouped["pos_review_count"]/temp_grouped["total_review_count"]*100,2)
         temp_grouped.drop('predicted_sentiment', axis=1, inplace=True)
+
+        # filter out top 5 products and update column names
         sorted_top_5 = temp_grouped.sort_values('pos_sentiment_percent', ascending=False)[0:5]
         top_5_products = pd.merge(
             train[['name', 'brand', 'manufacturer']].drop_duplicates(),
@@ -40,16 +46,17 @@ def get_sentiment_recommendations(user):
         top_5_products.index = np.arange(1, len(top_5_products)+1)
         top_5_products.columns.name = 'S. No.'
 
+        # return formatted html response
         return dataframe_to_html(top_5_products)
 
     else:
         return f"Username {user} doesn't exist"
 
+
 def dataframe_to_html(df):
     """
     Convert the dataframe into a nice presentable table for the end user
     """
-
     html_resp = df.to_html().replace(
         'table border="1"', 'table border="1" style="border-collapse:collapse"'
     ).replace(
